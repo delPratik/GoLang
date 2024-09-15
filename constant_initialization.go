073729aa0f930166f32b1c306636aec2df618e82
delPratik <d3lpratik@gmail.com>
2024-09-15 20:12:21 +0545

    Constant Initialization in GO

---
+++ constant_initialization.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	const pi = 3.14
+
+	// Constants in Go cannot be initialized using a short assignment operator
+
+	fmt.Printf("The value of Pi is %v", pi)
+}
