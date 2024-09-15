584debdae96e4f04986bb5acf00dc3a93ea73a1d
delPratik <d3lpratik@gmail.com>
2024-09-15 20:06:01 +0545

    Variable initialization in GO

---
+++ variable_initialization.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var name = "Pratik"
+	var age = 23
+	Langugage := "GO"
+	// := also known as short assignment operator is also used for variable initialization and assignment in GO.
+	fmt.Printf("My name %s.  My age is %v. I am Learning %s. ", name, age, Langugage)
+}
